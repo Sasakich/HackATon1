691e6f89f18b06e1701bd7412c31ed84deebf3b6
Artemiy <timtimk30@yandex.ru>
2024-03-30 13:41:42 +0300

    db simple server

--- back/index.js
+++ back/index.js
@@ -1,17 +1,26 @@
 const http = require('http');
+const sqlite3 = require("sqlite3");
+const {open} = require("sqlite");
+const path = require('path')
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    console.log("GET")
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Access-Control-Allow-Origin' , '*')
-    res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+(async () => {
+    const db = await open({
+        filename: path.resolve(__dirname, './database/database.db'),
+        driver: sqlite3.Database
+    })
+    // console.log(db)
+    let a = await db.get('SELECT * FROM tbl');
+    console.log(a);
+})()
