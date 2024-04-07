85ca8ca9b97c7dec504bb8caa14ba88aab12342a
kalim <kali.mar.100101@gmail.com>
2024-04-07 16:16:58 +0300

    add scroll for chat

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,97 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import InputForm from './components/InputForm';
-import { Avatar, List } from "antd";
+import {Avatar, List} from "antd";
+import {UserItem} from "./Type/Type";
+import VirtualList from 'rc-virtual-list';
 
-function App() {const data = [
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
+function App() {
+    /*Избавиться от копипасты, с помощью передачи в Message нужных props*/
+    const [viewportHeight, setViewportHeight] = useState<number>(window.innerHeight);
 
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
+    const fakeDataUrl =
+        'https://randomuser.me/api/?results=20&inc=name,gender,email,nat,picture&noinfo';
+    const [data, setData] = useState<UserItem[]>([]);
 
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-];
-  return (
-    <div className="App">
-      <List style= {{width:'auto', flexDirection: 'row', minWidth:'40%'}}
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={(item, index) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-              title={<a href="https://ant.design">{item.title}</a>}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            />
-          </List.Item>
-        )}
-      />
-      <InputForm />
-    </div>
-  );
+    useEffect(() => {
+        const handleResize = () => {
+            setViewportHeight(window.innerHeight);
+        };
+        window.addEventListener('resize', handleResize);
+        appendData();
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const onScroll = (e: React.UIEvent<HTMLElement, UIEvent>) => {
+        if (Math.abs(e.currentTarget.scrollHeight - e.currentTarget.scrollTop - viewportHeight + 52) <= 1) {
+            appendData();
+        }
+    };
+
+    const appendData = () => {
+        fetch(fakeDataUrl)
+            .then((res) => res.json())
+            .then((body) => {
+                setData(data.concat(body.results));
+                // message.success(`${body.results.length} more items loaded!`);
+            });
+    };
+
+
+    return (
+        <div className="App">
+            {/*Информация о чатах*/}
+            <List style={{width: 'auto', flexDirection: 'row', minWidth: '40%'}}>
+                <VirtualList
+                    data={data}
+                    height={viewportHeight - 52}
+                    itemHeight={47}
+                    itemKey="email"
+                    onScroll={onScroll}
+                >
+                    {(item: UserItem) => (
+                        <List.Item key={item.email}>
+                            <List.Item.Meta
+                                avatar={<Avatar src={item.picture.large}/>}
+                                title={<a href="https://ant.design">{item.name.last}</a>}
+                                /*Здесь будет последнее сообщение*/
+                                description={item.email}
+                            />
+                            <div>Content</div>
+                        </List.Item>
+                    )}
+                </VirtualList>
+            </List>
+
+            <InputForm/>
+        </div>
+    );
 }
 
 export default App;
+
+
