a079509dc864f2ee4d3809c1c8e384c51d28370c
kalim <kali.mar.100101@gmail.com>
2024-04-13 13:42:28 +0300

    add effector

--- frontend/my-app/src/components/Modal.tsx
+++ frontend/my-app/src/components/Modal.tsx
@@ -1,50 +1,48 @@
-import React, { useState, ReactNode } from 'react';
+import React, { FC, useState, ReactNode } from 'react';
+import {$password, $user, $userInput, buttonSubmit, passwordChange, userChange} from "../models/init";
+import {useUnit} from "effector-react"
 
 interface ModalProps {
     isOpen: boolean;
     onClose: () => void;
-    children: ReactNode;
+
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+const Modal: FC<ModalProps> = ({ isOpen, onClose }) => {
+    const [username, password] = useUnit([$userInput, $password]);
 
     const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
+        userChange(event.target.value);
     };
 
     const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
+        passwordChange(event.target.value);
     };
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
-        const url = 'http://localhost:3001/api/users';
+        buttonSubmit();
         console.log("done1");
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ login: username, password: password }),
-            });
 
-            console.log("gotten resp");
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
+            // const response = await fetch(url, {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({ login: username, password: password }),
+            // });
+            //
+            // console.log("gotten resp");
+            // if (!response.ok) {
+            //     throw new Error('Network response was not ok');
+            // }
 
-            const data = await response.json();
-            console.log('Success:', data);
+
+
             onClose();
-        } catch (error) {
-            console.error('Error:', error);
-        }
         console.log("done");
     };
-    if (!isOpen) return null;
+    // if (!isOpen )return null;
 
     return (
         <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
