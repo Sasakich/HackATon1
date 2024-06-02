2fefb925fcd12b8578db091df79f730f7f03dd90
Tea-O <kali.mar.100101@gmail.com>
2024-06-02 14:41:12 +0300

    fix modal

--- src/server/controllers/user/index.ts
+++ src/server/controllers/user/index.ts
@@ -1,19 +1,27 @@
-import { Router, Request, Response } from 'express';
+import {Router, Request, Response} from 'express';
+import sqlite3 from "sqlite3";
+import { open } from 'sqlite';
 
 export const userRoute: Router = Router();
 
 const names = [
-  'Сэр Корвалол',
-  'Рыцарь-о-пончик',
-  'Лунный танцор',
-  'Солнечный зайчик',
-  'Капитан Подсолнух',
-  'Гусеница-путешественница',
-  'Шоколадный магнат',
+    'Сэр Корвалол',
+    'Рыцарь-о-пончик',
+    'Лунный танцор',
+    'Солнечный зайчик',
+    'Капитан Подсолнух',
+    'Гусеница-путешественница',
+    'Шоколадный магнат',
 ];
 
-userRoute.get('/api/me', (_req: Request, res: Response) => {
-  const name = names[new Date().getDay()];
 
-  res.json({ name });
-});
+    userRoute.get('/api/me',  (_req: Request, res: Response) => {
+      const name = names[new Date().getDay()];
+
+      // const user = await db.get(
+      //     'SELECT * FROM users WHERE login = ? AND password = ?',
+      //     ['test1', 'test1']
+      // );
+      res.json({name});
+    });
+
